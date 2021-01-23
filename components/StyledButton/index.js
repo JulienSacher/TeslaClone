c2231bc2dc97a1029c7338977e9428e6c32dee96
[Julien Sacher] <[julien.sacher@hotmail.fr]>
2021-01-23 22:04:40 +0100

    Ajout du projet TeslaClone

---
+++ components/StyledButton/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Text, View, Pressable } from 'react-native';
+import styles from './styles';
+
+const StyledButton = (props) => {
+
+    const { type, content, onPress } = props;
+
+    const backgroundColor = type === 'primary' ? 'black' : 'white';
+    const textColor = type === 'primary' ? 'white' : 'grey';
+
+    return (
+        <View style={styles.container}>
+            <Pressable
+                style={[styles.button, {backgroundColor: backgroundColor}]}
+                onPress={() => onPress()}
+            >
+                <Text style={[styles.text, {color: textColor}]}>{content}</Text>
+            </Pressable>
+        </View>
+    );
+};
+
+
+export default StyledButton;
